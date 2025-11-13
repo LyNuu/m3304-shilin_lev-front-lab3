a3a94b3b14b837870df5967f3eff232b15132e3e
Shilin Lev <413115@niuitmo.ru>
2025-11-13 22:02:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,15 +11,8 @@ export default defineConfig({
 				main: resolve(__dirname, "index.html"),
 				catalog: resolve(__dirname, "catalog.html"),
 				began: resolve(__dirname, "began.html"),
-<<<<<<< HEAD
 				notes: resolve(__dirname, "notes.html")
 			}
 		}
 	}
-=======
-				notes: resolve(__dirname, "notes.html"),
-			},
-		},
-	},
->>>>>>> c8f836ac699269592fe981f5b9283e427a15e263
 });
